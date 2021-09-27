c785260b3e283dd376ad96a5b0b619f62d26d4c3
FelipeBaranovsky <fbaranovsky603@alumnos.iua.edu.ar>
2021-09-27 15:43:24 -0300

    eje6

--- Archivos/ej6.py
+++ Archivos/ej6.py
@@ -1,2 +1,22 @@
 '''6. Modiﬁcar el programa anterior para que imprima la frecuencia de repeti-
 ción de cada palabra en el archivo'''
+
+palabras = dict()
+
+try:
+    file = open("Archivos//file.txt",mode = "r", encoding = "utf-8")
+    lines = file.readlines()
+    print(lines)
+
+    for line in lines:
+        for word in line.split(" "):
+            if palabras.get(word) == None:
+                palabras.update({word:1})
+            else:
+                palabras.update({word:palabras.get(word)+1})
+ 
+    print(palabras)
+    file.close()
+    
+except:
+    print("No se pudo abrir el archivo")
---
+++ Archivos/ej7.py
@@ -0,0 +1,3 @@
+'''7. Escribir un programa en Python que lea información de texto de cuatro
+archivos distintos, la imprima por pantalla y luego la guarde en un archivo
+distinto.'''
