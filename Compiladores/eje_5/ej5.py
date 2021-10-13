2de74690f13e0b25af9338357c7e6b16a9844c76
maliendo106 <maliendo106@alumnos.iua.edu.ar>
2021-10-13 18:11:55 -0300

    linking dinamico

--- Compiladores/eje_5/ej5.py
+++ Compiladores/eje_5/ej5.py
@@ -3,4 +3,20 @@
 talarla en el sistema y extraer conclusiones sobre el tamaño del archivo
 resultante
 
+
+$ gcc -Wall -fPIC -c sumar.c
+$ gcc -Wall -fPIC -c restar.c
+$ gcc -Wall -c main.c
+# con esto genero los .o
+
+$ ld -o libreriadhs.so sumar.o restar.o -shared
+#tenemos la libreria dinamica; com gcc en lugar de ld anda tambien
+
+$ sudo cp libreriadhs.so /usr/lib/libreriadhs.so $ldconfig
+# y ahora la movemos a /usr/lib no me preguntes xq pero es asi
+
+$ gcc -o example main.o -I libreriadhs
+$ ./example 
+#generamos el ejecutable y c'est fini
+
 '''
